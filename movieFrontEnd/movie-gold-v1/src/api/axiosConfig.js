da816ae8db68abb3ddcec9c6dc12964f12ec0be0
Gary Ma <gary.ma@entelect.co.za>
2024-05-19 15:00:14 +0200

    Not working...

--- movieFrontEnd/movie-gold-v1/src/api/axiosConfig.js
+++ movieFrontEnd/movie-gold-v1/src/api/axiosConfig.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-expoert default axios.create({
-  baseURL: 'http://9c96-103-106-239-104.ap.ngrok.io/',
-  headers: {"ngrok-skip-browser-warning": "true"}
+export default axios.create({
+  //baseURL: 'http://9c96-103-106-239-104.ap.ngrok.io/',
+  baseURL: 'http://localhost:8080/',
+  //headers: {"ngrok-skip-browser-warning": "true"},
+  headers: {"Access-Control-Allow-Origin": "true"}
 })

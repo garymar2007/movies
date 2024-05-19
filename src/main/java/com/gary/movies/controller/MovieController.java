df2e0f4ee14acbab2e9243a5c50e175350c4cb45
Gary Ma <gary.ma@entelect.co.za>
2024-05-19 08:54:02 +0200

    Add another endpoint for getting a single movie

--- src/main/java/com/gary/movies/controller/MovieController.java
+++ src/main/java/com/gary/movies/controller/MovieController.java
@@ -2,14 +2,18 @@ package com.gary.movies.controller;
 
 import com.gary.movies.models.Movie;
 import com.gary.movies.service.MovieService;
+import jakarta.websocket.server.PathParam;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -20,5 +24,12 @@ public class MovieController {
     public ResponseEntity<List<Movie>> getAllMovies() {
 
         return new ResponseEntity<List<Movie>>(movieService.getAllMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    //NB: @PathParam is from JAX-RS and @PathVariable is from Spring.  So @PathParam is not recognized by Spring,
+    // but only by JAX-RS for REST web service, whist @PathVariable is recognized by Spring and REST API.
+    public ResponseEntity<Optional<Movie>> getMovieById(@PathVariable("id") ObjectId id) {
+        return new ResponseEntity<Optional<Movie>>(movieService.getMovieById(id), HttpStatus.OK);
     }
 }
--- src/main/java/com/gary/movies/service/MovieService.java
+++ src/main/java/com/gary/movies/service/MovieService.java
@@ -2,10 +2,12 @@ package com.gary.movies.service;
 
 import com.gary.movies.models.Movie;
 import com.gary.movies.repository.MovieRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -13,5 +15,9 @@ public class MovieService {
     private MovieRepository movieRepository;
     public List<Movie> getAllMovies() {
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> getMovieById(ObjectId id) {
+        return movieRepository.findById(id);
     }
 }
